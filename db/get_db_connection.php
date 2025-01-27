cdcdb9756f3cb999f4a1190ec77cab4d0d1aad8c
TheCharlesChristy <charleschristy325@gmail.com>
2025-01-27 15:11:16 +0000

    Refactor database connection to use config file and add .gitignore for sensitive data

--- db/get_db_connection.php
+++ db/get_db_connection.php
@@ -1,20 +1,24 @@
 <?php
-$host = getenv(name: 'DB_HOST');
-$db   = getenv(name: 'DB_NAME');
-$user = getenv(name: 'DB_USER');
-$pass = getenv(name: 'DB_PASS');
+$config = include 'config.php';
+
+$host = $config['DB_HOST'];
+$db   = $config['DB_NAME'];
+$user = $config['DB_USER'];
+$pass = $config['DB_PASS'];
 
 try {
+    // Connect to the database
     $pdo = new PDO(
         dsn: "mysql:host=$host;dbname=$db;charset=utf8mb4",
         username: $user,
         password: $pass
     );
+    // Set the PDO error mode to exception
+    // If an error occurs, it will throw an exception
     $pdo->setAttribute(
         attribute: PDO::ATTR_ERRMODE,
         value: PDO::ERRMODE_EXCEPTION
     );
-    // Connection established
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
